a74ccc14395205e314778cc92dce8aed512c9002
A.M.Kharazi <a.m.kharazi.ac@gmail.com>
2024-03-18 19:55:48 +0330

    updated

--- Utils/Cifar10_loader.py
+++ Utils/Cifar10_loader.py
@@ -2,18 +2,24 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 
-def get_cifar10_dataloaders(data_dir, transform=None, batch_size=64, image_size=192):
-    if transform is None:
-        transform = transforms.Compose([
+def get_cifar10_dataloaders(data_dir='../datasets', transform_train=None, transform_test=None, batch_size=64, image_size=192):
+    if transform_train is None:
+        transform_train = transforms.Compose([
             transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(image_size, padding=4),
+            transforms.RandomCrop(32, padding=2),
+            transforms.Resize((image_size, image_size)), 
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    if transform_test is None:
+        transform_train = transforms.Compose([
             transforms.Resize((image_size, image_size)), 
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
 
-    train_dataset = datasets.CIFAR10(root=data_dir, train=True, transform=transform, download=False)
-    test_dataset = datasets.CIFAR10(root=data_dir, train=False, transform=transform, download=False)
+    train_dataset = datasets.CIFAR10(root=data_dir, train=True, transform=transform_train, download=False)
+    test_dataset = datasets.CIFAR10(root=data_dir, train=False, transform=transform_test, download=False)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
