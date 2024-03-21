0df1beabaeef295186d5f1d96f52d8951d018b35
A.M.Kharazi <a.m.kharazi.ac@gmail.com>
2024-03-21 19:55:00 +0330

    updated

---
+++ Utils/Accuracy_measures.py
@@ -0,0 +1,6 @@
+import torch
+
+def topk_accuracy(output, target, k):
+    _, topk_indices = torch.topk(input=output, k=k, dim=1, largest=True, sorted=True)
+    correct = topk_indices.eq(target.view(-1, 1).expand_as(topk_indices)).sum()
+    return (correct / output.shape[0]) * 100.0
