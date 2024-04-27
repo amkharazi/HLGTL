9582fdd9d21804292fe110c646ae3e2591d256b6
amkharazi <a.m.kharazi.ac@gmail.com>
2024-04-27 23:39:51 +0330

    updated-server

--- Tests/run_script.sh
+++ Tests/run_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python3 TEST_037.py
-python3 TEST_038.py
-python3 TEST_039.py
-python3 TEST_040.py
-python3 TEST_041.py
-python3 TEST_042.py
-python3 TEST_043.py
-python3 TEST_044.py
-python3 TEST_045.py
-python3 TEST_046.py
-python3 TEST_047.py
-python3 TEST_048.py
+python3 TEST_049.py
+python3 TEST_050.py
+python3 TEST_051.py
+python3 TEST_052.py
+python3 TEST_053.py
+python3 TEST_054.py
+python3 TEST_055.py
+python3 TEST_056.py
+python3 TEST_057.py
+python3 TEST_058.py
+python3 TEST_059.py
+python3 TEST_060.py
 
