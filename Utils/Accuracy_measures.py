c837305faa94ec2480922863bb51245117ccd57e
amkharazi <a.m.kharazi.ac@gmail.com>
2024-05-21 20:15:07 +0330

    topk accuracy

--- Utils/Accuracy_measures.py
+++ Utils/Accuracy_measures.py
@@ -1,6 +1,24 @@
+# Author: A.M.Kharazi
+# License: BSD 3 clause
+
 import torch
 
 def topk_accuracy(outputs, targets, topk=(1,)):
+    '''
+    calculates top-k accuracy 
+
+    ----------
+    outputs : torch.tensor
+        
+    targets : torch.tensor
+
+    topk  : tuple
+        calculates top k accuracy given outpurs and targets
+
+    
+    reutrns a python dictionary of top i <= k accuracies 
+
+    '''
     maxk = max(topk)
     _, topk_indices = torch.topk(input=outputs, k=maxk, dim=1, largest=True, sorted=True)
     correct = topk_indices.eq(targets.view(-1, 1).expand_as(topk_indices))
