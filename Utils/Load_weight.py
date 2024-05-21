eb6d051b61304f4d5237de11fb98c088912a5d9a
amkharazi <a.m.kharazi.ac@gmail.com>
2024-05-21 20:30:12 +0330

    load weights

--- Utils/Load_weight.py
+++ Utils/Load_weight.py
@@ -1,3 +1,17 @@
+# Author: A.M.Kharazi
+# License: BSD 3 clause
+
 import torch
+
 def load_weight(model, weight_path):
+    '''
+    loads weights for a model
+
+    ----------
+    model : torch.nn.Module
+        your pytorch model or layer
+
+    weight_path : str
+        path to your .pth saved weights file
+    '''
     return model.load_state_dict(torch.load(weight_path))
