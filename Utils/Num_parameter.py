e649b822215060223b15aea48141b1514148ff7d
amkharazi <a.m.kharazi.ac@gmail.com>
2024-05-21 20:31:46 +0330

    number of parameters

--- Utils/Num_parameter.py
+++ Utils/Num_parameter.py
@@ -1,3 +1,12 @@
+# Author: A.M.Kharazi
+# License: BSD 3 clause
 # Counts the number of parameters in a nn.Module object
 def count_parameters(model):
+    '''
+    Counts the number of parameters
+
+    ----------
+    model : torch.nn.Module
+        your pytorch model or layer
+    '''
     return sum(p.numel() for p in model.parameters())
